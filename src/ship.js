e1e45396453f091a95d37eea008a944a13927f3e
Yato Aki <yatoakii@gmail.com>
2022-06-03 13:17:15 +0630

    Ship Factory function added

--- src/ship.js
+++ src/ship.js
@@ -1,6 +1,16 @@
 
-function Hello() {
-  return -1;
+const Ship = (length,hits = [],sunk = false) => {
+  const hit = (location) => {
+    hits.push(location);
+  }
+
+  const issunk = () => {
+    let answer = [...Array(length).keys()].join(',') === hits.sort().join(',')
+    sunk = answer;
+    return sunk;
+  }
+
+  return {length,hits,sunk,hit,issunk};
 }
 
-export default Hello;
+export default Ship
--- test/ship.test.js
+++ test/ship.test.js
@@ -1,7 +1,49 @@
-import Hello from "../src/ship.js"
+import Ship from "../src/ship.js"
 
-test("Is Working?", () =>{
-  expect(Hello()).toBe(-1);
+describe("Ship Functions", () => {
+
+  let testShip;
+  beforeEach(() => {
+    testShip = Ship(5);
+  })
+
+  test("Have default hits attribute", () => {
+    expect(testShip.hits).toEqual([]);
+  })
+
+  test("Have default sunk attrtiute", () => {
+    expect(testShip.sunk).toBe(false);
+  })
+
+  test("Have length", () => {
+    expect(testShip.length).not.toBeUndefined();
+  })
+
+  test("Get hit", () => {
+    testShip.hit(2);
+    expect(testShip.hits).toEqual([2]);
+  })
+
+  test("Get multiple hits", () => {
+    testShip.hit(2);
+    testShip.hit(4);
+    expect(testShip.hits).toEqual([2,4]);
+  })
+
+  test("is Sunk? - True", () => {
+    testShip.hit(0);
+    testShip.hit(1);
+    testShip.hit(2);
+    testShip.hit(3);
+    testShip.hit(4);
+    expect(testShip.issunk()).toBe(true);
+  })
+
+  test("is Sunk? - False", () => {
+    testShip.hit(1);
+    testShip.hit(2);
+    testShip.hit(3);
+    expect(testShip.issunk()).toBe(false);
+  })
+
 })
-
-test.todo("Okay")
