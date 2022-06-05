da909857dd34d304fdc1f001d04ee7fc658388fd
Yato Aki <yatoakii@gmail.com>
2022-06-05 13:00:18 +0630

    Change Game Class to object literal

--- src/script/index.js
+++ src/script/index.js
@@ -2,23 +2,15 @@ import '../styles/style.css'
 import UI from "./ui.js"
 import Player from "./player.js"
 
-const shipSizes = [5,4,3,3,2];
-
-class Game{
-  constructor(){
-    this.player = Player();
-    this.ai = Player();
-    this.currentInputLength = 5;
-    this.currentHorizontal = true;
-  }
-
-  start(){
-    UI.loadPage();
-  }
+const Game = {
+    player: Player(),
+    ai: Player(),
+    start:() => {
+      UI.loadPage();
+    }
 }
 
-const game = new Game();
-game.start();
+Game.start();
 
 
 
